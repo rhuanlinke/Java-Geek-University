9b0625e3a62f91dd57b51d739170e107bb3016cc
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-07-05 16:40:01 -0300

    Revisando as variáveis

---
+++ src/Programa01.java
@@ -0,0 +1,20 @@
+//Variáveis
+public class Programa01 {
+
+	public static void main(String[] args) {
+		//Declaração de variável
+		int valor;
+		//Declaração de variáveis
+		int num1, num2, num3;
+		//Declarando e inicializado algumas variáveis
+		int num4, num5 = 4, num6 = 7, nu8;
+		
+		char caractere;
+		float preco = 6;
+		
+		float _valor = 34.2f; //Por padrão os valores de ponto flutuante sã double no JAVA
+		
+		double _valor2 = 34.2;
+	}
+
+}
