efc32eed21032ae45936b95e96b408b390792cc6
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-07-07 20:54:18 -0300

    Create Programa12.java
    Tipos numericos Reais primarios e não primarios

---
+++ src/Programa12.java
@@ -0,0 +1,17 @@
+//Tipos de dados
+//Numericos (Reais)
+public class Programa11 {
+
+	public static void main(String[] args) {
+		//Tipos primários/primitivos:
+		//Por padrão os numeros reais em Java são considerados double
+		dobule preco1 = 23.4;
+		float preco2 = 4.1f; //Realf
+		
+		//Tipos não primários/primitivos
+		Float preco3 = 44.5f;
+		Double preco4 = 44.5;
+		
+	}
+
+}
