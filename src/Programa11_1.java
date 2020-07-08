2b1e56b46aa8bc20415591a6d8a12aeb531d1c83
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-07-08 20:59:29 -0300

    Revisando conhecimento
    Está com erro

---
+++ src/Programa11_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+//Revisando conhecimento
+public class Programa11_1 {
+	public static void main(String[] args) {
+		Integer aluno, professor[], turma, i, y;
+		Scanner teclado = new Scanner(System.in); 
+		
+		System.out.println("Quantos professores a turma tem?");
+		y = teclado.nextInt();
+		
+		for(i=0;i<y;i++) {
+			System.out.println("Qual seu código de professor?");
+			professor[i] = teclado.nextInt();
+		}
+		
+		for(i=0;i<y;i++) {
+			System.out.println(professor[i]);
+		}
+		
+		
+	}
+}
