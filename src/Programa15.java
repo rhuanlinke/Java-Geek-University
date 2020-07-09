d494aae6ae5baa36689c946b25d262d16b63a986
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-07-08 21:44:47 -0300

    Operações Matemáticas e CAST
    Para utilizar um cast deve definir a variavel com o tipo primitivo antes dela.
    int num1=1;
    int num2=2;
    float resposta;
    
    resposta = (float)num1/(float) //(1 / 2)
    
    //Se fosse INT a resposta seria 0, mas utilizando o CAST para float, a resposta vai ser 0.5

---
+++ src/Programa15.java
@@ -0,0 +1,22 @@
+//Operações matemáticas
+public class Programa15 {
+
+	public static void main(String[] args) {
+		int num1 = 5, num2 = 9, res;
+		float res2, num3=11f, num4 =14f;
+		
+		//soma
+		res = num1 + num2;
+		System.out.println("A soma de "+ num1 + "+" + num2 + " é " +(num1+num2)+" ou "+res);
+		
+		//Divisão (inteira)
+		res = num2 / num1;
+		System.out.println(res);
+		res = (int)num4 / (int)num3;
+		System.out.println(res); //resultado seria float 1.27
+		
+		//Divisão (real)
+		res2 = (float)num2 / (float)num1; //cast vai pegar uma variavel INT e usa-la como se fosse (float)
+	}
+
+}
