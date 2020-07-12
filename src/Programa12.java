d0b0bef2df04c4ba366747c3882ed43103a32cf9
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-07-12 16:10:31 -0300

    Update Programa12.java
    Correção de bug (nome da classe tem que ser o mesmo nome do arquivo JAVA.

--- src/Programa12.java
+++ src/Programa12.java
@@ -1,11 +1,11 @@
 //Tipos de dados
 //Numericos (Reais)
-public class Programa11 {
+public class Programa12 {
 
 	public static void main(String[] args) {
 		//Tipos primários/primitivos:
 		//Por padrão os numeros reais em Java são considerados double
-		dobule preco1 = 23.4;
+		double preco1 = 23.4;
 		float preco2 = 4.1f; //Realf
 		
 		//Tipos não primários/primitivos
