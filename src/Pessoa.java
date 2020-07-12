b9561570d6000c90d46ee6bf2cf9f1e09b4c06b8
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-07-12 16:25:55 -0300

    Métodos (funções da classe)
    Métodos
    /* - Podemos entender os métodos como a ação que é realizada por um objeto da classe;
     * - Podemos entender também que os métodos são comportamentos dos objetos da classe;
     * - Mesmos requisitos para funções:
     * 		a)Tipo de retorno (Tipo de dado que a função vai retornar);
     * 		b) Nome - Corresponde a ação que a função realiza;
     * 		c) Parâmetros/Argumentos de entrada (opcional);
     * 		d) Retorno (Opcional - depende do tipo de retorno);
     * - DIFERENÇAS: Método faz parte de uma classe. Função não!

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,6 +1,8 @@
 //Classes
 
 //Atributos
+
+//Métodos
 public class Pessoa {
 	String nome, email, endereco_rua, endereco_bairro, endereco_complemento;
 	int ano_nascimento, endereco_numero, endereco_cep;
--- src/Produto.java
+++ src/Produto.java
@@ -8,12 +8,28 @@
   } */
 
 //Atributos
-/* São as características da classe/molde/modelo de dados; 
- * Podemos entender atributos/estados como variáveis da classe;
- * Atributos são nomeados em letras minusculas, sem carac. espec.*/
+/* - São as características da classe/molde/modelo de dados; 
+ * - Podemos entender atributos/estados como variáveis da classe;
+ * - Atributos são nomeados em letras minusculas, sem carac. espec.*/
+
+//Métodos
+/* - Podemos entender os métodos como a ação que é realizada por um objeto da classe;
+ * - Podemos entender também que os métodos são comportamentos dos objetos da classe;
+ * - Mesmos requisitos para funções:
+ * 		a)Tipo de retorno (Tipo de dado que a função vai retornar);
+ * 		b) Nome - Corresponde a ação que a função realiza;
+ * 		c) Parâmetros/Argumentos de entrada (opcional);
+ * 		d) Retorno (Opcional - depende do tipo de retorno);
+ * - DIFERENÇAS: Método faz parte de uma classe. Função não!
+ */
 
 public class Produto {
 	String nome;
 	float preco, desconto;
+	
+	//Método para aumentar o preço do produto em R$10
+	void aumentar_preco(float valor) {
+		this.preco = this.preco + valor;
+	}
 	
 }
