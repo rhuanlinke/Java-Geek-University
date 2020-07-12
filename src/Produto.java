66855c6f13063b376fd6ca55f9a13ee4227b8597
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-07-12 15:59:28 -0300

    Classe produto
    Observações sobre classe
    
    - O nome das classes inicia cada palavra com letra maiuscula;
     * - N�o cont�m: acentua��o, caracteres especiais, espa�o;
     * - Nas classes Java, n�o existe a implementa��o da fun��o main();
     * - O nome da classe Java DEVE ser o mesmo nome do arquivo java;
     * - Toda classe Java possui a seguinte forma:
     public class NomeDaClasse{

---
+++ src/Produto.java
@@ -0,0 +1,13 @@
+//Classes
+/* - O nome das classes inicia cada palavra com letra maiuscula;
+ * - Não contém: acentuação, caracteres especiais, espaço;
+ * - Nas classes Java, não existe a implementação da função main();
+ * - O nome da classe Java DEVE ser o mesmo nome do arquivo java;
+ * - Toda classe Java possui a seguinte forma:
+ public class NomeDaClasse{
+  } */
+
+public class Produto {
+	
+	
+}
