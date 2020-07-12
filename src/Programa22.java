3b9ebfc1bb2d362f8ced4d263d1acdbaa1e1b527
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-07-12 17:45:25 -0300

    Metodo Construtor
    * - Construtor de uma classe SEMPRE tem o mesmo nome da classe;
     * - Por padr�o, a JVM (Java Virtual Machine) cria em tempo de execu��o
    um construtor padr�o - um construtor vazio (com nenhum par�metro de entrada);
     * - Podemos ter mais de um construtor na classe;

--- src/Programa22.java
+++ src/Programa22.java
@@ -12,7 +12,7 @@ public class Programa22 {
 			
 		//Declaração e inicializando/instanciando o objeto
 		//p1 = instância do objeto / objeto
-		Produto p1 = new Produto();
+		Produto p1 = new Produto(); //Construtor
 		p1.nome = "Notebook";
 		p1.preco = 2.367f;
 		p1.desconto = 15.0f;
@@ -22,7 +22,7 @@ public class Programa22 {
 		System.out.println("R$"+p1.preco);
 		System.out.println(p1.desconto+"%\n");
 		
-		p0 = new Produto ();
+		p0 = new Produto (); //Construtor
 		p0.nome = "Caneta Bic";
 		p0.preco = 2.14f;
 		p0.desconto = 5;
@@ -31,7 +31,7 @@ public class Programa22 {
 		System.out.println("R$"+p0.preco);
 		System.out.println(p0.desconto+"%\n");
 		
-		Pessoa pessoa1 = new Pessoa();
+		Pessoa pessoa1 = new Pessoa(); //Construtor
 		pessoa1.nome = "Angelina Jolie";
 		pessoa1.email = "jolie@gmail.com";
 		pessoa1.ano_nascimento = 1976;
